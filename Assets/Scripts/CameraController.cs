ee1bead8163bcd70f71b9f1eb0a9ebce401a8e38
Aalok Tanwar <aalok.singh@vitbhopal.ac.in>
2022-04-05 21:56:29 +0530

    coingame

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform cameraTarget;
+    public Transform lookTarget;
+    public float sSpeed = 80.0f;
+    public Vector3 dist;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        // transform.position = player.transform.position + distance;
+        // transform.rotation = player.transform.rotation;
+        Vector3 dPos = cameraTarget.position + dist;
+        Vector3 sPos = Vector3.Lerp(transform.position, dPos, sSpeed * Time.deltaTime);
+        transform.position = sPos;
+        transform.LookAt(lookTarget.position);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+public class PlayerController : MonoBehaviour
+{
+    public TextMeshProUGUI countText, winText, timeText, loseText;
+    // private Rigidbody rb;
+    public float speed = 20.0f;
+    public float turnSpeed = 50.0f;
+    private int count;
+    public float timeRemaining = 10;
+    
+    void Start()
+    {
+        // rb = GetComponent<Rigidbody>();
+        count = 0;
+        Debug.Log(SystemInfo.deviceUniqueIdentifier);
+    }
+
+    private void Update() {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * moveVertical);
+
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * moveHorizontal);
+
+        if(timeRemaining > 0 && count < 37) {
+            timeRemaining -= Time.deltaTime;
+            if(timeRemaining < 0) timeRemaining = 0;
+        }
+        else if (timeRemaining == 0)
+        {
+            loseText.gameObject.SetActive(true);
+            winText.gameObject.SetActive(false);
+        }
+
+        timeText.text = "Time : " + timeRemaining.ToString();
+        
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.gameObject.CompareTag("coin"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            countText.text = "Score : " + count.ToString();
+            if(count >= 37){
+                winText.gameObject.SetActive(true);
+                loseText.gameObject.SetActive(false);
+            }
+        }
+    }
+
+}
